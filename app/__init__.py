71e5520ec238be27383ee9d84acdf5201eccddeb
kelvin onkundi ndemo <32582512+kelvinndmo@users.noreply.github.com>
2018-11-09 11:02:04 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from instance.config import app_config
 from flask_jwt_extended import JWTManager
-<<<<<<< HEAD
 from .admin.admin_views import CompleteOrder, AcceptStatus, MarkOrderInTransit, DeclineOrder
-
-=======
-<<<<<<< HEAD
 from app.auth.auth_views import Login, SignUp
-=======
 from .customer.customer_views import PostParcel, GetOrders, SpecificOrder, InTransitOrders, GetAcceptedOrders, DeclinedOrders, CompletedOrders
 
->>>>>>> a8686befc68819fa446d8017cf2d5ee07641ef1c
->>>>>>> challenge-tw-develop
 
 
 jwt = JWTManager()
@@ -29,18 +22,13 @@ def create_app(config_stage):
 
     api = Api(app)
 
-<<<<<<< HEAD
+
     api.add_resource(CompleteOrder, '/api/v1/orders/<int:id>/completed')
     api.add_resource(AcceptStatus, '/api/v1/orders/<int:id>/approved')
     api.add_resource(MarkOrderInTransit, '/api/v1/orders/<int:id>/intransit')
     api.add_resource(DeclineOrder, '/api/v1/orders/<int:id>/declined')
-=======
-<<<<<<< HEAD
     api.add_resource(SignUp, '/api/v1/auth/signup')
     api.add_resource(Login, '/api/v1/auth/login')
-    
-=======
- 
     api.add_resource(SpecificOrder, '/api/v1/orders/<int:id>')
     api.add_resource(PostParcel, '/api/v1/placeorder/orders')
     api.add_resource(GetOrders, '/api/v1/orders')
@@ -48,7 +36,6 @@ def create_app(config_stage):
     api.add_resource(CompletedOrders, '/api/v1/orders/completedorders')
     api.add_resource(DeclinedOrders, '/api/v1/orders/declined')
     api.add_resource(InTransitOrders, '/api/v1/orders/intransit')
->>>>>>> a8686befc68819fa446d8017cf2d5ee07641ef1c
->>>>>>> challenge-tw-develop
+
 
     return app
